62da1023f6c9a0f6a2177d210250d7277e808efa
久保出雅俊 <kubode@team-lab.com>
2015-10-31 22:11:32 +0900

    Rename to Knife

---
+++ app/src/main/kotlin/sandbox/kotlin/teamlab/com/kotlinsandbox/Knife.kt
@@ -0,0 +1,119 @@
+package sandbox.kotlin.teamlab.com.kotlinsandbox
+
+import android.app.Activity
+import android.app.Application
+import android.app.Fragment
+import android.content.Context
+import android.view.View
+import java.util.*
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
+import android.support.v4.app.Fragment as SupportFragment
+
+public interface HasObjectGraph {
+    var objectGraph: ObjectGraph
+}
+
+private class NotFoundException(detailMessage: String?) : RuntimeException(detailMessage)
+
+public class ObjectGraph(private val parent: ObjectGraph? = null) {
+    private val graph: MutableMap<Class<*>, MutableMap<String?, Wrapper<Any>>> = HashMap()
+
+    private fun <V : Any> add(clazz: KClass<V>, name: String? = null, type: Type, creator: () -> V) {
+        graph.getOrPut(clazz.java, { HashMap() }).put(name, type.createWrapper(creator))
+    }
+
+    public fun <V : Any> provide(clazz: KClass<V>, creator: () -> V, name: String? = null) {
+        add(clazz, name, Type.NORMAL, creator)
+    }
+
+    public fun <V : Any> provideSingleton(clazz: KClass<V>, creator: () -> V, name: String? = null) {
+        add(clazz, name, Type.SINGLETON, creator)
+    }
+
+    public fun <V : Any> get(clazz: KClass<V>, name: String? = null): V {
+        @Suppress("UNCHECKED_CAST")
+        return graph[clazz.java]?.get(name)?.value as V?
+                ?: parent?.get(clazz, name)
+                ?: throw NotFoundException("${clazz.java}(${name ?: "No name"}) is not found.")
+    }
+
+    private enum class Type(val createWrapper: (() -> Any) -> Wrapper<Any>) {
+        NORMAL({ NormalWrapper(it) }), SINGLETON({ SingletonWrapper(it) })
+    }
+
+    private interface Wrapper<V> {
+        val value: V
+    }
+
+    private class NormalWrapper<V>(private val creator: () -> V) : Wrapper<V> {
+        override val value: V
+            get() = creator()
+
+        override fun toString(): String {
+            return "$value"
+        }
+    }
+
+    private class SingletonWrapper<V>(creator: () -> V) : Wrapper<V> {
+        override val value: V by lazy { creator() }
+        override fun toString(): String {
+            return "$value(Singleton)"
+        }
+    }
+
+    override fun toString(): String {
+        return "{parent: $parent, graph: {$graph}}"
+    }
+}
+
+private fun Context.findObjectGraph(): ObjectGraph {
+    if (this is HasObjectGraph) {
+        return this.objectGraph
+    }
+    val application = this.applicationContext
+    if (application is HasObjectGraph) {
+        return application.objectGraph
+    }
+    throw  NotFoundException("${ObjectGraph::class.java.simpleName} is not found in $this.")
+}
+
+private val Application.objectGraph: ObjectGraph by Lazy { it.findObjectGraph() }
+
+private val Activity.objectGraph: ObjectGraph by Lazy { it.application.findObjectGraph() }
+
+private val Fragment.objectGraph: ObjectGraph by Lazy { it.activity.findObjectGraph() }
+
+private val SupportFragment.objectGraph: ObjectGraph by Lazy { it.activity.findObjectGraph() }
+private val View.objectGraph: ObjectGraph by Lazy { it.context.findObjectGraph() }
+
+public fun <V : Any> Application.inject(clazz: KClass<V>, name: String? = null)
+        = lazy { objectGraph.get(clazz, name) }
+
+public fun <V : Any> Activity.inject(clazz: KClass<V>, name: String? = null)
+        = lazy { objectGraph.get(clazz, name) }
+
+public fun <V : Any> Fragment.inject(clazz: KClass<V>, name: String? = null)
+        = lazy { objectGraph.get(clazz, name) }
+
+public fun <V : Any> SupportFragment.inject(clazz: KClass<V>, name: String? = null)
+        = lazy { objectGraph.get(clazz, name) }
+
+public fun <V : Any> View.inject(clazz: KClass<V>, name: String? = null)
+        = lazy { objectGraph.get(clazz, name) }
+
+// internal lazy
+private class Lazy<T, V>(private val initializer: (T) -> V) : ReadOnlyProperty<T, V> {
+    private object NO
+
+    @Suppress("UNCHECKED_CAST")
+    private var value: V = NO as V
+
+    override fun getValue(thisRef: T, property: KProperty<*>): V {
+        if (value == NO) {
+            value = initializer(thisRef)
+        }
+        return value
+    }
+}
--- app/src/main/kotlin/sandbox/kotlin/teamlab/com/kotlinsandbox/MyApplication.kt
+++ app/src/main/kotlin/sandbox/kotlin/teamlab/com/kotlinsandbox/MyApplication.kt
@@ -12,16 +12,18 @@ public class MyApplication : Application(), HasObjectGraph {
     val namedInt2: Int by inject(Int::class, "named2")
     val singletonDouble1: Double by inject(Double::class)
     val singletonDouble2: Double by inject(Double::class)
+    val singletonCharSeq: CharSequence by inject(CharSequence::class)
     override fun onCreate() {
         super.onCreate()
         val s = System.nanoTime()
         val parent = ObjectGraph()
-        parent.add(String::class, null, ObjectGraph.Type.NORMAL, { "normal(${Math.random()})" })
-        parent.add(Int::class, "named1", ObjectGraph.Type.SINGLETON, { 1 })
-        parent.add(Int::class, "named2", ObjectGraph.Type.SINGLETON, { 2 })
+        parent.provide(String::class, { "normal(${Math.random()})" })
+        parent.provideSingleton(Int::class, { 1 }, "named1")
+        parent.provideSingleton(Int::class, { 2 }, "named2")
         val objectGraph = ObjectGraph(parent)
-        objectGraph.add(Int::class, "named2", ObjectGraph.Type.SINGLETON, { 22222 })
-        objectGraph.add(Double::class, null, ObjectGraph.Type.SINGLETON, { Math.random() })
+        objectGraph.provideSingleton(Int::class, { 22222 }, "named2")
+        objectGraph.provideSingleton(Double::class, { Math.random() })
+        objectGraph.provideSingleton(CharSequence::class, { "${objectGraph.get(String::class)}aaa" })
         this.objectGraph = objectGraph
 
         Log.v("App", "$normalString1")
@@ -32,6 +34,8 @@ public class MyApplication : Application(), HasObjectGraph {
         Log.v("App", "$namedInt2")
         Log.v("App", "$singletonDouble1")
         Log.v("App", "$singletonDouble2")
+        Log.v("App", "$singletonCharSeq")
         Log.v("App", "${System.nanoTime() - s}")
+        Log.v("App", "$objectGraph")
     }
 }
